9adfa3390cb747981f32acbcb42e198513a85c44
James Fong <51034737+meiyaismywaifu@users.noreply.github.com>
2020-07-13 22:44:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,15 @@ function breakOut(array, changeValue, stopValue){
 }
 
 function keepGoing(array, changeValue, skipValue){
-
+  for(var i = 0; i < array.length; i++){
+    if(array[i] === skipValue){
+      continue // since we're not doing anything this could just be blank, no if condition.
+    }
+    else{
+      array[i] = changeValue;
+    }
+  }
+  return array;
 }
 
 function findBy(array, findFn){
